e757be807f4aaa0a0f8e919a148ad5df77177447
yrom1 <ryanm.inbox@gmail.com>
2022-03-15 14:03:57 -0400

    Fix: Use `/usr/$USER` path when parsing `pyproject.toml`

--- parse-toml.py
+++ parse-toml.py
@@ -1,9 +1,14 @@
-import tomli
+import getpass
 import pipes
 
-with open('pyproject.toml', 'r') as f:
-    toml_str = f.read()
+import tomli
 
-toml_dict = tomli.loads(toml_str)
-type_checker = toml_dict['tool']['ty']['type_checker']
-print(f"export TY_TYPE_CHECKER={pipes.quote(type_checker)}")
+try:
+    with open(f"/usr/{getpass.getuser()}/pyproject.toml", "r") as f:
+        toml_str = f.read()
+
+    toml_dict = tomli.loads(toml_str)
+    type_checker = toml_dict["tool"]["ty"]["type_checker"]
+    print(f"export TY_TYPE_CHECKER={pipes.quote(type_checker)}")
+except:
+    print(f'export TY_TYPE_CHECKER=""')
