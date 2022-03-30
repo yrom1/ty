8470b1eec57054b926ce009d707c24dcc9a9796e
yrom1 <ryanm.inbox@gmail.com>
2022-03-30 10:05:49 -0400

    Run through Black

--- make-toml.py
+++ make-toml.py
@@ -1,8 +1,9 @@
 import sys
+
 ENGINE = sys.argv[1]
 TOML = f"""\
 [tool.ty]
 type_checker = "{ENGINE}"
 """
-with open('pyproject.toml', 'w') as f:
+with open("pyproject.toml", "w") as f:
     f.write(TOML)
